a3610a5841de0809cda9ad460b4b492441e5376d
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2020-04-24 19:31:23 +0530

    test

--- NewProject1.py
+++ NewProject1.py
@@ -1,19 +1,18 @@
 import requests
 
-
 def handler(event, context):
-    
+
     try:
         res = requests.get(
-            "http://demo.fintechsandpit.com/contact-for-access",
-            params={},
-            headers={"Accept":""}
+            "https://api.stripe.com/v1/account",
+            params={"expand":"time"},
+            headers={"Accept":"application/json"}
         )
-        print(res)
         # your code goes here
+        print(res)
     except BaseException as e:
         # error handling goes here
-        print(e)
         raise(e)
+        print(e)
   
     return {"message": "Successfully executed"}
